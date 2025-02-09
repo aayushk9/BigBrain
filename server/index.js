d6725ce3b128845e7b7f0cbd94598d660b342dec
aayushk9 <ayushkokate1@gmail.com>
2025-02-09 17:58:16 +0530

    hash passwords and start research route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const zod = require("zod");
+const zod = require("zod");  
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -8,6 +8,8 @@ const app = express();
 const port = 3000;
 const SECRET_KEY = process.env.JWT_SECRET
 const URI = process.env.MONGO_URI
+const User = require("./models/userModel");  
+const ResearchPaper = require("./models/ResearchPaper");
 
 app.use(express.json());
 
@@ -15,15 +17,11 @@ mongoose.connect(URI)
     .then(() => console.log(`Database connected`))
     .catch((err) => console.error(err))
 
-const users = mongoose.model("users", ({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-}))
+app.post("/signup", async (req, res) => {   
 
-app.post("/signup", async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const { username, password } = req.body;
 
+    // zod schema for validating user input
     const signinSchema = zod.object({
         username: zod.string().min(1, "username is required"),
         password: zod.string()
@@ -33,6 +31,7 @@ app.post("/signup", async (req, res) => {
             .regex(/[\W_]/, "password must contain atleast one special character")
     });
 
+    // using that schema check if data is valid
     const result = signinSchema.safeParse({
         username, password
     });
@@ -41,29 +40,30 @@ app.post("/signup", async (req, res) => {
         return res.status(400).json({ error: result.error.format() });
     }
 
+    const usernameExists = await User.findOne({ username });
+    if(usernameExists){
+        const comparePassword = await bcrypt.compare(password, usernameExists.password)
+        if(usernameExists && comparePassword){
+            return res.json({   
+                msg: "Please go to login route to login"
+            })
+        } else {
+            return res.json({
+                msg: "Username already taken"
+            })
+        }
+    }
+
     const saltRounds = 10;
-    const hashedpassword = await bcrypt.hash(password, saltRounds);
-    const userExists = await users.findOne({ username, password });
-    const usernameExists = await users.findOne({ username });
-
-    if (usernameExists) {
-        return res.json({
-            msg: "Username already taken"
-        })
-    }
-
-    if (userExists) {
-        return res.json({
-            msg: "Please go to login route"
-        })
-    }
-
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const token = jwt.sign({ username }, SECRET_KEY);
-    const user = new users({
+
+    const user = new User({
         username: username,
-        password: hashedpassword
+        password: hashedPassword
     })
-    await user.save();
+
+    await user.save();     
     return res.json({
         msg: "user added to database",
         token: `${token}`
@@ -73,8 +73,7 @@ app.post("/signup", async (req, res) => {
 
 app.post("/login", async (req, res) => {
 
-    const username = req.body.username;
-    const password = req.body.password;
+    const { username, password} = req.body;  
 
     const loginSchema = zod.object({
         username: zod.string().min(1, "username should have atleast one character"),
@@ -85,27 +84,35 @@ app.post("/login", async (req, res) => {
             .regex(/[\W_]/, "password must contain at least one special character")
     })
 
+    // making sure enter valid data
     const result = loginSchema.safeParse({
         username: username,
         password: password
     })
 
     if (!result.success) {
-        return res.json({
+        return res.status(400).json({
             msg: "Please enter valid data"
         })
     }
 
-    const userExists = await users.findOne({ username });
-
-    if (!userExists) {
-        return res.json({
+    // check for correct password
+    const usernameExists = await User.findOne({ username });
+    if (!usernameExists) {
+        return res.status(404).json({
             msg: "User does not exist, please go to signin route"
         })
     }
 
+    const comparePassword = await bcrypt.compare(password, usernameExists.password)
+    if(!comparePassword){
+        return res.status(401).json({
+            msg: "Please enter correct password"
+        })
+    }
+
     const token = jwt.sign({ username }, SECRET_KEY);
-    return res.json({
+    return res.status(200).json({
         msg: `Token generated ${token}`
     })
 });
@@ -113,6 +120,31 @@ app.post("/login", async (req, res) => {
 app.delete("/logout", (req, res) => {
 
 });
+
+app.post("/research", (req, res) => {
+    const searchForPaper = req.body.searchForPaper;
+    /******
+     * 
+     *let that user see this content for multiple times but after that tell them to signup
+      so than use jwt verify to verify the token and authenticate
+
+     we need to compare the data in our database with the data the user has requested
+     using text index we will be searching for the keywords entered by the user and compare it with the keywords
+     in our database so the papers which match will be shared
+     the paper which matches the most should be at the top
+     * 
+     * If no match in DB, then:
+
+            Scrape research papers from the web (e.g., Google Scholar, arXiv, PubMed).
+            Extract important keywords from the scraped papers.
+            Store those keywords & links in the database for future searches.
+            Return the scraped paper to the user.
+    
+     * 
+     * 
+     * **********/ 
+    
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
