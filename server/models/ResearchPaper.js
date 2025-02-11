f76d452d2cd4fb113991b15108d48d387650bcdc
aayushk9 <ayushkokate1@gmail.com>
2025-02-11 21:43:26 +0530

    update research schema

--- server/models/ResearchPaper.js
+++ server/models/ResearchPaper.js
@@ -3,15 +3,27 @@ const { Schema } = mongoose;
 
 
 const researchSchema = new Schema({
+    type: {
+        type: String,
+        enum: ["Research Paper", "Technical Article", "Whitepaper"],
+        required: true
+    },   
     title: String,
-    abstract: String,
+    abstract: String,   
     keywords: String,
     author: String,
     link: String,
     topic: String,
-    source: String,  
-    publishedDate: Date
+    source: String,    
+    category: {
+        type: String,
+        enum: ["web3", "startups"],  // Only allow these categories
+        required: true
+    }
 }) 
+  
+// This tells MongoDB that these fields should be used for efficient text searching
+researchSchema.index({title: "text", keywords: "text", topic: "text", abstract: "text"});  
 
-const ResearchPaper = mongoose.model("ResearchPaper", researchSchema);
-module.exports = ResearchPaper;
+const researchPaper = mongoose.model("ResearchPaper", researchSchema);
+module.exports = researchPaper;
