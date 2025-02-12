7343786b52fe4d3360a84cc370aff81ae2fd7746
aayushk9 <ayushkokate1@gmail.com>
2025-02-12 20:56:23 +0530

    implement fast search

--- server/scraper/scraper.js
+++ server/scraper/scraper.js
@@ -1,27 +1,32 @@
 const researchScraper = require("./web3/researchScraper");
-const technicalPaperScraper = require("./web3/technicalScraper");
 
 const scrapeAll = async (category, userInput) => {
     try {
         let results = [];
 
-        if (category == "web3") {
-            const [research,technical] = await Promise.all([
-                researchScraper(userInput),  
-                //technicalPaperScraper(userInput)
-            ]);
-
-            results = [
-                ...research.map((item) => ({ ...item, type: "Research paper" }))
-                //...technical.map((item) => ({ ...item, type: "Technical articles" }))
-            ]
-
+        if (category == "web3") {  
+                const [research] = await Promise.all([
+                    researchScraper(userInput),  
+                ]);
+  
+                results = research.map((item) => ({
+                    type: "Research Paper",  
+                    title: item.title || "Unknown Title",
+                    abstract: item.abstract || "No abstract available",
+                    keywords: item.keywords || "", 
+                    author: Array.isArray(item.authors) ? item.authors.join(", ") : item.authors || "Unknown Author",
+                    link: item.link || "#",
+                    topic: item.topic || "General", 
+                    source: item.source || "Unknown",
+                    category: category
+                }));
+    
         } else if (category == "startups") {
-            const [research, eg1, eg2] = await Promise.all([
+            const [eg0, eg1, eg2] = await Promise.all([
                 // scrapers
             ])
             results = [
-                ...research.map((item) => ({ ...item, type: "Research" })),
+                ...eg0.map((item) => ({ ...item, type: "eg0" })),
                 ...eg1.map((item) => ({ ...item, type: "eg1" })),
                 ...eg2.map((item) => ({ ...item, type: "eg2" }))
             ]
@@ -32,11 +37,11 @@ const scrapeAll = async (category, userInput) => {
         }
         return results;
 
-    } catch (err) {
+    } catch (err) {   
         console.error(err);
     }
 }
 
 
-module.exports = scrapeAll;
+module.exports = scrapeAll; 
 
