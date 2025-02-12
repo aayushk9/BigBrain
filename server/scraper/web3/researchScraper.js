351b42eca648f5770375f5570891de4b7dd2a3a6
aayushk9 <ayushkokate1@gmail.com>
2025-02-12 20:58:03 +0530

    fix: parsing data instead of scraping

--- server/scraper/web3/researchScraper.js
+++ server/scraper/web3/researchScraper.js
@@ -1,8 +1,39 @@
 const axios = require("axios");
-const xml2js = require("xml2js"); // Convert XML response to JSON
+const xml2js = require("xml2js"); 
+const Bottleneck = require("bottleneck");
+const redis = require("redis");
+
+
+const client = redis.createClient();  
+client.on("error", (err) => console.error("Redis Client Error:", err));
+
+(async() => {
+    try{   
+      await client.connect();
+      console.log("Redis conneccted successfully")
+    } catch(err){
+       console.log(`Redis connection failed ${err}`);   
+    }   
+})
+
+const limiter = new Bottleneck({
+    minTime: 15000 // 15 seconds between requests (arXiv's rule)
+});
 
 const fetchArxivPapers = async (userInput) => {
-    try {
+    return limiter.schedule(async () => {
+    try {    
+
+        if (!client.isOpen) {
+            console.log("⏳ Reconnecting Redis...");
+            await client.connect();
+        }
+
+        const cachedData = await client.get(userInput);
+        if(cachedData){
+            console.log("Serving from cache");
+            return JSON.parse(cachedData) // returning the data in our cache
+        }
         const url = `http://export.arxiv.org/api/query?search_query=${encodeURIComponent(userInput)}&start=0&max_results=10`;
 
         const { data } = await axios.get(url);
@@ -11,21 +42,34 @@ const fetchArxivPapers = async (userInput) => {
         const result = await parser.parseStringPromise(data);
         const entries = result.feed.entry || [];
 
+        console.log("Fetching from arXiv...");
         let papers = entries.map((entry) => ({
-            title: entry.title,
+            type: "Research Paper", 
+            title: entry.title,  
             abstract: entry.summary,
-            authors: Array.isArray(entry.author) 
-                ? entry.author.map(a => a.name) 
-                : [entry.author.name],  // Handle single/multiple authors
+            keywords: "", 
+            author: Array.isArray(entry.author)  
+                ? entry.author.map(a => a.name).join(", ")
+                : entry.author.name,    
             link: entry.id,    
-            source: "arXiv"
-        }));
-
+            topic: "General", 
+            source: "arXiv",
+            category: "web3" 
+        })); 
+        
+        await client.setEx(userInput, 900, JSON.stringify(papers)); // storing new requests (papers) in redis for 15 minutes
+        console.log("Fetched from arvix and cached");
         return papers;
-    } catch (error) {
+    } catch (error) {  
         console.error("Error fetching from arXiv API:", error);
         return [];
     }
+});
 };
+
+process.on("exit", () => {
+    client.quit();
+    console.log("Redis client closed");
+});   
 
 module.exports = fetchArxivPapers;
