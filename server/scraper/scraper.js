39d3cea2933773e02cd887f377db89e01c35668a
aayushk9 <ayushkokate1@gmail.com>
2025-02-13 05:38:51 +0530

    remove else if

--- server/scraper/scraper.js
+++ server/scraper/scraper.js
@@ -1,47 +1,27 @@
 const researchScraper = require("./web3/researchScraper");
 
-const scrapeAll = async (category, userInput) => {
+const scrapeArvix = async (userInput) => {
     try {
         let results = [];
-
-        if (category == "web3") {  
                 const [research] = await Promise.all([
                     researchScraper(userInput),  
                 ]);
   
                 results = research.map((item) => ({
                     type: "Research Paper",  
-                    title: item.title || "Unknown Title",
-                    abstract: item.abstract || "No abstract available",
-                    keywords: item.keywords || "", 
-                    author: Array.isArray(item.authors) ? item.authors.join(", ") : item.authors || "Unknown Author",
-                    link: item.link || "#",
-                    topic: item.topic || "General", 
-                    source: item.source || "Unknown",
-                    category: category
+                    title: item.title,  
+                    author: Array.isArray(item.authors) ? item.authors.join(", ") : item.authors,
+                    link: item.link,
+                    source: item.source,
                 }));
     
-        } else if (category == "startups") {
-            const [eg0, eg1, eg2] = await Promise.all([
-                // scrapers
-            ])
-            results = [
-                ...eg0.map((item) => ({ ...item, type: "eg0" })),
-                ...eg1.map((item) => ({ ...item, type: "eg1" })),
-                ...eg2.map((item) => ({ ...item, type: "eg2" }))
-            ]
-        } else {
-            return res.json({
-                msg: "No data found"
-            })
-        }
         return results;
-
+    
     } catch (err) {   
         console.error(err);
     }
 }
 
 
-module.exports = scrapeAll; 
 
+module.exports = scrapeArvix; 
