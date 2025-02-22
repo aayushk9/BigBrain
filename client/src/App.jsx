28c636ed12e1d8d7bd5288d89928b617c60bc443
aayushk9 <ayushkokate1@gmail.com>
2025-02-22 08:50:13 +0530

    app changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,48 @@
-import {Routes, Route, BrowserRouter } from "react-router-dom";
-import { Context } from './components/Context';  
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Context } from './components/Context';
 import { Login } from './components/Login';
-import {Signup} from './components/Signup';
-import {Research} from './components/Research';
-import {CryptoNews} from './components/CryptoNews';
+import { Signup } from './components/Signup';
+import { Research } from './components/Research';
+import { CryptoNews } from './components/CryptoNews';
 import { Startups } from './components/Startups';
 import { Navbar } from './components/Navbar';
-import {ResearchSearch} from './components/ResearchSearch';
+import { ResearchSearch } from './components/ResearchSearch';
 import { CryptoLists } from "./components/CryptoLists";
 import { StartupsData } from "./components/StartupsData";
 
+
 function App() {
-  return (  
-    <BrowserRouter>
-    <img src="/logo.png" alt="logo"/>    
+  return (
+    <div className="bg-black min-h-screen w-full">
+      <BrowserRouter>
+        <Routes>   
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/research/search" element={<ResearchSearch />} />
+          <Route path="/cryptonews" element={<CryptoLists />} />
+          <Route path="/startupnews" element={<StartupsData />} />
 
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/research/search" element ={<ResearchSearch/>}/>
-      <Route path="/cryptonews" element={<CryptoLists/>}/>
-      <Route path="/startupnews" element = {<StartupsData/>}/>
+          <Route
+            path="/"
+            element={
+              <>
 
-      <Route
-        path="/"
-        element={
-          <>
-            <Navbar/>
-            <Context />
-            <Research />    
-            <CryptoNews />   
-            <Startups />   
-          </>  
-        }
-      />
-    </Routes>
-  </BrowserRouter>
-  
+                <Navbar />
+                <div className="flex flex-col items-center justify-start min-h-screen">
+                  <Context />
+                  <div className="flex flex-row gap-4 mt-4">
+                    <CryptoNews />
+                    <Research />
+                    <Startups />
+                  </div>
+                </div>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
-export default App  
+export default App       
