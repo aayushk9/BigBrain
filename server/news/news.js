65e9fddbea1e124d27dae405027f2e5b9e0e9b99
aayushk9 <ayushkokate1@gmail.com>
2025-02-23 12:12:49 +0530

    redis

--- server/news/news.js
+++ server/news/news.js
@@ -38,7 +38,7 @@ const fetchNews = async () => {
         ])
 
         const allNews = [...telegraphNews, ...bitcoinNews, ...cryptoSlateNews, ...journalNews,]
-        //await client.setEx(cacheKey, JSON.stringify(allNews));
+        await client.setEx(cacheKey, JSON.stringify(allNews));
         return allNews;
 
     } catch (err) {
